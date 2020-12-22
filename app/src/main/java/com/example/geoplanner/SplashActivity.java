f5bdd86a8184a7b1ce5bf3eb57538c7d59ab18f5
teamgeoplanner <teamgeoplanner@gmail.com>
2020-12-22 14:25:34 +0530

    made some changes

---
+++ app/src/main/java/com/example/geoplanner/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.example.geoplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView logoText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        logo = findViewById(R.id.imgLogo);
+        logoText = findViewById(R.id.txtLogo);
+
+        logo.setAnimation(topAnim);
+        logoText.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+    }
+}
