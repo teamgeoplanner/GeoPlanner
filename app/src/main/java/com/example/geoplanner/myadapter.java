d5d167cc83a6611fc44add2add62e1b21a4b2656
teamgeoplanner <teamgeoplanner@gmail.com>
2021-02-22 14:31:28 +0530

    made some new changes

--- app/src/main/java/com/example/geoplanner/myadapter.java
+++ app/src/main/java/com/example/geoplanner/myadapter.java
@@ -1,8 +1,11 @@
 package com.example.geoplanner;
 
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -12,6 +15,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class myadapter extends FirebaseRecyclerAdapter<model, myadapter.myviewholder> {
 
@@ -20,14 +25,29 @@ public class myadapter extends FirebaseRecyclerAdapter<model, myadapter.myviewho
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull myviewholder holder, int position, @NonNull final model model) {
-        holder.taskName.setText("○  "+model.getTName());
+    protected void onBindViewHolder(@NonNull myviewholder holder, final int position, @NonNull final model model) {
+        holder.taskName.setText(model.getTName());
 
         holder.taskClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AppCompatActivity activity = (AppCompatActivity) view.getContext();
                 activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragments_container, new TaskDetailFragment(model.getTName())).addToBackStack(null).commit();
+            }
+        });
+
+        holder.taskName.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if(compoundButton.isChecked()) {
+                    
+                }
+
+                else {
+
+                }
+
             }
         });
     }
@@ -41,7 +61,7 @@ public class myadapter extends FirebaseRecyclerAdapter<model, myadapter.myviewho
 
     public class myviewholder extends RecyclerView.ViewHolder {
 
-        TextView taskName;
+        CheckBox taskName;
         RelativeLayout taskClick;
 
         public myviewholder(@NonNull View itemView) {
